d26c73e408ea60969ea7b18297235623d0d6fab0
Codecamp <Codecamp@codecamp-mac-mini-84.local>
2016-07-25 11:49:23 +0200

    Initial Commit

---
+++ TamagotchiIOS16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TamagotchiIOS16
+//
+//  Created by Codecamp on 25.07.16.
+//  Copyright © 2016 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TamagotchiIOS16/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TamagotchiIOS16
+//
+//  Created by Codecamp on 25.07.16.
+//  Copyright © 2016 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
