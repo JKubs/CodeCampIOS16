6a140f286a74618907c69a0e5f7aa1bcd35ccd52
Steffen <Codecamp@Codecamp-Mac-mini-9.local>
2016-07-28 11:26:48 +0200

    some stuff on PushNotifications

--- TamagotchiIOS16/Model/Loader.h
+++ TamagotchiIOS16/Model/Loader.h
@@ -16,6 +16,6 @@
 
 +(BOOL)loadSaveStateTo:(GameViewController *)controller;
 +(NSString*)loadLastUsedSlot;
-+(NSArray*)loadSavedNotificationsFromSlot:(NSString*)saveSlot;
++(NSMutableArray*)loadSavedNotificationsFromSlot:(NSString*)saveSlot;
 
 @end
